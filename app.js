86a690d721fd52eb85c7c0174f7acd4c1dcabee5
Ice <icey.ggjt@gmail.com>
2022-06-13 22:38:25 +0800

    change timeStyle

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-if("serviceWorker" in navigator) {
+if("serviceWorker" in navigator && location.protocol == "https:") {
   navigator.serviceWorker.register("./sw.js", {scope: "/"});
 }
 function getTime() {
   document.getElementsByTagName("p")[0].innerText = new Intl.DateTimeFormat((navigator.language || "en-US"), {
     dateStyle: "full",
-    timeStyle: "full",
+    timeStyle: "medium",
   }).format(new Date());
 }
 getTime();
